a44ff79d691e64aad4e07dd267c1f9432ae7470e
fantusya <100998437+fantusya@users.noreply.github.com>
2022-10-08 21:51:48 +0000

    Deploying to gh-pages from @ fantusya/Filmoteka@6a536a88a645d65612be12ad7f8f804153df0262 🚀

--- library.0f9d0498.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".watched"),c=document.querySelector(".queue");function t(){console.log("Click!!!!!!!!!!")}e.addEventListener("click",t),c.addEventListener("click",t),modalWatched.addEventListener("click",t),modalQueue.addEventListener("click",t);
-//# sourceMappingURL=library.0f9d0498.js.map
--- library.322e37bf.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".watched"),c=document.querySelector(".queue");function t(){console.log("Click!!!!!!!!!!")}e.addEventListener("click",t),c.addEventListener("click",t),modalWatched.addEventListener("click",t),modalQueue.addEventListener("click",t)}();
-//# sourceMappingURL=library.322e37bf.js.map
