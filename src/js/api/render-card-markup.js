c185d86df9691368b6f7cbd4779552c8c8df3a85
Ivan Roschin <ivan.roschin86@gmail.com>
2022-10-08 21:13:44 +0300

    add cards style

--- src/js/api/render-card-markup.js
+++ src/js/api/render-card-markup.js
@@ -36,24 +36,33 @@ const BASE_POSTER_URL = `https://image.tmdb.org/t/p/w500`;
 // }
 
 export default function makingMarkup(results) {
-  const markup = results.map(({ title, name, poster_path, genre_ids }) => {
-    return `<div class="movie-card">
+  const markup = results
+    .map(
+      ({
+        title,
+        name,
+        poster_path,
+        genre_ids,
+        release_date,
+        first_air_date,
+        vote_average,
+      }) => {
+        return `<li class="movie-card">
                 <img class="movie-card__img" src="${BASE_POSTER_URL}/${poster_path}" alt="" loading="lazy"/>
-                <div class="info">
-                    <p class="info-item">
-                        <b>${title || name}</b>
+                  <h2 class="movie-card__title">${title || name}</h2>
+                    <p class="movie-card__info">
+                        <b>${gettingGenresList(genre_ids)} </b>
                     </p>
-                    <p class="info-item">
-                        <b>Views ${gettingGenresList(genre_ids)}</b>
+                    <p class="movie-card__info">
+                    conslo
+                        <b>${release_date || first_air_date}</b>
                     </p>
-                    <p class="info-item">
-                        <b>Comments</b>
+                       <p class="movie-card__raiting">
+                        <b>${vote_average}</b>
                     </p>
-                    <p class="info-item">
-                        <b>Downloads</b>
-                    </p>
-                </div>
-            </div>`;
-  });
+            </li>`;
+      }
+    )
+    .join('');
   return markup;
 }
