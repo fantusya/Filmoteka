5abfa8e95be21af1f42cf3b83560171958100d9f
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-10-11 20:43:15 +0300

    Фиксит запись в локал сторедж

--- src/js/local_storage.js
+++ src/js/local_storage.js
@@ -2,17 +2,19 @@ import { refs } from './refs';
 import makingMarkup from './api/render-card-markup';
 import { insertFilmsMarkupToLibrary } from './api/insertingIntoDifferentContainers';
 
-const arrayFilmsWatched = [];
 const arrayFilmsQueue = [];
 
-
 export function addWatchedLocalStorage(obj) {
+  let arrayFilmsWatched = [];
+  if (localStorage.getItem('watched')) {
+    arrayFilmsWatched = JSON.parse(localStorage.getItem('watched'));
+  }
+
   arrayFilmsWatched.push(obj);
 
   localStorage.setItem('watched', JSON.stringify(arrayFilmsWatched));
   return arrayFilmsWatched;
 }
-
 
 export function addQueueLocalStorage(obj) {
   arrayFilmsQueue.push(obj);
@@ -21,22 +23,21 @@ export function addQueueLocalStorage(obj) {
   return arrayFilmsQueue;
 }
 
-
 export function getWatchedFilms() {
   clearLibrary();
   try {
     const saveFilms = localStorage.getItem('watched');
     //Якщо в localStorage немає ключа watched - показуємо заглушку
-          if(saveFilms === null) {
-            addScreenSaver();
-            return;
-           }
+    if (saveFilms === null) {
+      addScreenSaver();
+      return;
+    }
 
     const parsedFilms = JSON.parse(saveFilms);
-     if(parsedFilms.length === 0) {
-            addScreenSaver();
-            return;
-      }
+    if (parsedFilms.length === 0) {
+      addScreenSaver();
+      return;
+    }
 
     const renderWatched = makingMarkup(parsedFilms);
     insertFilmsMarkupToLibrary(renderWatched);
@@ -45,23 +46,22 @@ export function getWatchedFilms() {
   }
 }
 
-
 export function getQueueFilms() {
   clearLibrary();
   try {
     const saveFilms = localStorage.getItem('queue');
     //Якщо в localStorage немає ключа queue - показуємо заглушку
-          if (saveFilms === null) {
-            addScreenSaver();
-            return;
-          }
-          
+    if (saveFilms === null) {
+      addScreenSaver();
+      return;
+    }
+
     const parsedFilms = JSON.parse(saveFilms);
     // Перевірка на порожній масив в localStorage
-          if(parsedFilms.length === 0) {
-            addScreenSaver();
-            return;
-          }
+    if (parsedFilms.length === 0) {
+      addScreenSaver();
+      return;
+    }
 
     const renderQueue = makingMarkup(parsedFilms);
     insertFilmsMarkupToLibrary(renderQueue);
@@ -71,16 +71,16 @@ export function getQueueFilms() {
 }
 
 //   Фунуція для очищення попередніх результатів рендеру
-  function clearLibrary () {
-    refs.libraryCardsContainer.innerHTML = '';
-  }
+function clearLibrary() {
+  refs.libraryCardsContainer.innerHTML = '';
+}
 
-  // Функція для відмальовки "заглушки" (якщо localStorage порожній)
-  function addScreenSaver() {
-    refs.libraryCardsContainer.innerHTML = `<strong 
+// Функція для відмальовки "заглушки" (якщо localStorage порожній)
+function addScreenSaver() {
+  refs.libraryCardsContainer.innerHTML = `<strong 
     style="
     font-size: 18px;
     color: var(--secondary-text-cl);">
     Sorry, no information has been added
     </strong>`;
-  }
+}
