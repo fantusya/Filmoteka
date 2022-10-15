66c0e8117ddd2445572f7307b6e7c24729304964
Andriy Yarmosh <101985044+HeadOd@users.noreply.github.com>
2022-10-15 21:10:59 +0300

    key-change

--- src/js/api/moviesApiServiceClass.js
+++ src/js/api/moviesApiServiceClass.js
@@ -1,6 +1,8 @@
 import API_KEY from './constants/apiKeys';
 import { BASE_TRENDING_MOVIES_URL } from './constants/baseUrls';
 import { BASE_GENRE_URL } from './constants/baseUrls';
+//trailer
+import renderBadRequest from '.././searchFilmByKeyword/renderBadRequest';
 
 export default class MoviesApiService {
   constructor() {
@@ -39,6 +41,21 @@ export default class MoviesApiService {
       return response.json();
     });
   }
+
+  //FETCH TRAILER
+  fetchFilmTrailer(movie_id) {
+    const linkForSearchById = `
+    https://api.themoviedb.org/3/movie/${movie_id}/videos?api_key=${API_KEY}&language=en-US`;
+
+    return fetch(linkForSearchById).then(response => {
+      if (!response.success) {
+        renderBadRequest();
+        console.log('hi');
+        return;
+      }
+        return response.json();     
+    })
+  };
 
   setLanguage() {
     if (!localStorage.getItem('lang')) {
--- src/js/movie-trailer.js
+++ src/js/movie-trailer.js
@@ -1,9 +1,17 @@
-import fetchFilms from "./searchFilmByKeyword/fetch-trailer";
+import MoviesApiService from './api/moviesApiServiceClass';
 import renderTrailer from './searchFilmByKeyword/render-trailer';
+import renderBadRequest from './searchFilmByKeyword/renderBadRequest';
+
+const movieApiServise = new MoviesApiService();
 
 function onTrailerClickBtn(currentId) {
   try {
-      fetchFilms(currentId).then(films => {
+    movieApiServise
+      .fetchFilmTrailer(currentId).then(films => {
+        if (films.results.length === 0) {
+          renderBadRequest()
+          return;
+        }
         renderTrailer(films.results[0].key);
     });
   } catch (err) {
